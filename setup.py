2ee7ca899451713064a80ce1180306bc56321b47
Ganessh Kumar <rpganesshkumar@gmail.com>
2017-01-17 18:54:27 +0530

    Updating new tarball in setup.cfg

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='img-dl',
-    version='0.1',
+    version='0.2',
     description='CLI to download images',
     author='Ganessh Kumar R P',
     author_email='rpganesshkumar@gmail.com',
     url='https://github.com/ganesshkumar/imgdl',
-    download_url='https://github.com/ganesshkumar/imgdl/tarball/0.1',
+    download_url='https://github.com/ganesshkumar/imgdl/tarball/0.2',
     keywords=['image', 'downloader', 'imgur', 'image download'],
     py_modules=['imgdl'],
     install_requires=[
